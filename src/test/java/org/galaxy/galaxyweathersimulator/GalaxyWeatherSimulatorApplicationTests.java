d2bbcd88c3cc2157735cddd3dab321ac42007bca
Ivan Bedecarats <igedecarats@gmail.com>
2018-09-28 00:37:03 -0300

    Spring Initializr project layout

---
+++ src/test/java/org/galaxy/galaxyweathersimulator/GalaxyWeatherSimulatorApplicationTests.java
@@ -0,0 +1,16 @@
+package org.galaxy.galaxyweathersimulator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GalaxyWeatherSimulatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
