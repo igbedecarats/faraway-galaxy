d1c255c76eb97fa6c75956e906f2f448bc330442
Ivan Bedecarats <igbedecarats@gmail.com>
2018-09-30 12:36:12 -0300

    Added GlobalExceptionHandler.java

---
+++ src/main/java/org/galaxy/galaxyweathersimulator/GlobalExceptionHandler.java
@@ -0,0 +1,85 @@
+package org.galaxy.galaxyweathersimulator;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import javax.persistence.EntityNotFoundException;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+/**
+ * This class handles exceptions raised in any part of the external
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+  private static final Log logger = LogFactory.getLog(GlobalExceptionHandler.class);
+
+  /**
+   * Catch and log Spring MVC specific exceptions
+   */
+  @NotNull
+  @Override
+  protected ResponseEntity<Object> handleExceptionInternal(
+      final Exception ex, final Object body, final HttpHeaders headers, final HttpStatus status,
+      final WebRequest request) {
+    logger.error("Spring MVC exception occurred", ex);
+    return super.handleExceptionInternal(ex, body, headers, status, request);
+  }
+
+  @ExceptionHandler({EntityNotFoundException.class, EmptyResultDataAccessException.class})
+  @ResponseBody
+  public ResponseEntity<?> handleEntityNotFound(final Exception ex) {
+    return toResponseEntity(ex, HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler({IllegalArgumentException.class, NullPointerException.class})
+  @ResponseBody
+  public ResponseEntity<?> handleBadRequests(final Exception ex) {
+    return toResponseEntity(ex, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(IllegalStateException.class)
+  @ResponseBody
+  public ResponseEntity<?> handleIllegalStateRequests(final Exception ex) {
+    return toResponseEntity(ex, HttpStatus.CONFLICT);
+  }
+
+  private ResponseEntity<HttpApiError> toResponseEntity(
+      final Throwable throwable,
+      final HttpStatus httpStatus) {
+    log(throwable);
+    HttpApiError error = HttpApiError.create(httpStatus, throwable.getMessage());
+    return new ResponseEntity<>(error, new HttpHeaders(), error.getStatus());
+  }
+
+  private void log(final Throwable throwable) {
+    String errorMessage = throwable.getMessage();
+    logger.error(errorMessage, throwable);
+  }
+
+  @Getter
+  @AllArgsConstructor
+  public static class HttpApiError {
+
+    private HttpStatus status;
+
+    @JsonInclude(Include.NON_EMPTY)
+    private String message;
+
+    public static HttpApiError create(final HttpStatus status, final String message) {
+      return new HttpApiError(status, message);
+    }
+  }
+}
