a9947c17727c8e0d83fcf8bf2f54d0c91668b4c2
Ivan Bedecarats <igbedecarats@gmail.com>
2018-09-30 20:24:17 -0300

    Added javadoc to the forecast module

--- src/main/java/org/galaxy/galaxyweathersimulator/forecast/domain/Forecast.java
+++ src/main/java/org/galaxy/galaxyweathersimulator/forecast/domain/Forecast.java
@@ -8,25 +8,49 @@ import org.apache.commons.lang3.Validate;
 import org.galaxy.galaxyweathersimulator.weather.domain.Weather;
 import org.galaxy.galaxyweathersimulator.weather.domain.WeatherType;
 
+/**
+ * Aggregation of {@link Weather}s that were calculated for different days
+ */
 @NoArgsConstructor
 public class Forecast {
 
   private Map<Integer, Weather> forecast = new LinkedHashMap<>();
 
+  /**
+   * Adds a {@link Weather} to forecast for the given day.
+   *
+   * @param day The day of the weather being added to the forecast. It must be positive.
+   * @param weather The weather being added to the forecast for the given day. It cannot be null.
+   */
   public void addWeatherForDay(final int day, final Weather weather) {
     Validate.isTrue(day >= 0, "The day must be positive.");
     Validate.notNull(weather, "The weather cannot be null");
     forecast.put(day, weather);
   }
 
+  /**
+   * Returns the number of days in the forecast of the type {@link WeatherType#OPTIMAL}
+   *
+   * @return The number of days in the forecast of the type {@link WeatherType#OPTIMAL}. It's never negative.
+   */
   public long numberOfOptimalDays() {
     return numberOfDaysWithWeather(WeatherType.OPTIMAL);
   }
 
+  /**
+   * Returns the number of days in the forecast of the type {@link WeatherType#RAIN}
+   *
+   * @return The number of days in the forecast of the type {@link WeatherType#RAIN}. It's never negative.
+   */
   public long numberOfRainyDays() {
     return numberOfDaysWithWeather(WeatherType.RAIN);
   }
 
+  /**
+   * Returns the number of days in the forecast of the type {@link WeatherType#DROUGHT}
+   *
+   * @return The number of days in the forecast of the type {@link WeatherType#DROUGHT}. It's never negative.
+   */
   public long numberDroughtDays() {
     return numberOfDaysWithWeather(WeatherType.DROUGHT);
   }
--- src/main/java/org/galaxy/galaxyweathersimulator/forecast/service/SimulateForecastService.java
+++ src/main/java/org/galaxy/galaxyweathersimulator/forecast/service/SimulateForecastService.java
@@ -2,8 +2,16 @@ package org.galaxy.galaxyweathersimulator.forecast.service;
 
 import org.galaxy.galaxyweathersimulator.forecast.domain.Forecast;
 
+/**
+ * Forecast Simulator for the galaxy's solar system.
+ */
 public interface SimulateForecastService {
 
+  /**
+   * Simulates the forecast of the galaxy's solar system up to the given day.
+   * @param day The day up until the forecast will be simulated. It must be positive.
+   * @return The forecast of the galaxy's solar system up to the given day. It's never null.
+   */
   Forecast simulate(final int day);
 
 }
